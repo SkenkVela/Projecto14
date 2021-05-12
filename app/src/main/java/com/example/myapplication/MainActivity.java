4aa23704353b9521d1764690c69a246b3aaaa7da
SkenkVela <velaskenk@gmail.com>
2021-05-12 14:47:41 +0100

    Projecto 14

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    Button btn_abrirActivity;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        new Handler().postDelayed(() -> {
+            Intent _intent = new Intent(MainActivity.this,MainActivity2.class);
+            startActivity(_intent);
+        },5000);
+    }
+}
